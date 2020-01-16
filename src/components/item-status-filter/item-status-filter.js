5e0df38d9d48041b91f32fd8820378b130e85cc9
Mike <magrozero@yandex.ru>
2020-01-16 19:28:12 +0300

    fix

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,18 +1,31 @@
 import React, {Component} from "react";
 
 export default class ItemStatusFilter extends Component{
+
+  buttons = [
+    {name: 'all', label: 'All'},
+    {name: 'active', label: 'Active'},
+    {name: 'done', label: 'Done'},
+  ];
+
   render() {
+    const {filter, onFilter} = this.props;
+    const buttons = this.buttons.map(({name, label}) => {
+      console.log('btn', filter, name);
+      const isActive = filter === name;
+      const classViewBtn = isActive ? 'btn-info' : 'btn-outline-secondary';
+      return (
+        <button type='button'
+                className={`btn ${classViewBtn}`}
+                key={name}
+                onClick={() => onFilter(name)}>
+          {label}
+        </button>
+      )
+    });
     return (
       <div className="btn-group">
-        <button
-          type='button'
-          className="btn btn-info">All</button>
-        <button
-          type='button'
-          className="btn btn-outline-secondary">Active</button>
-        <button
-          type='button'
-          className="btn btn-outline-secondary">Done</button>
+        {buttons}
       </div>
     );
   }
